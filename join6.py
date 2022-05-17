51311093046b6001d1c145ada91d23c1c3ea7fed
Adam Masiarek <masiarek@gmail.com>
2022-05-17 15:57:20 -0400

    test

--- join6.py
+++ join6.py
@@ -1,5 +1,20 @@
 import csv
-ra =  csv.DictReader(open('a.csv'))
+from collections import defaultdict
+ra = csv.DictReader(open('a.csv'))
+rb = csv.DictReader(open('b.csv'))
+rc = csv.DictReader(open('c.csv'))
 all_keys = set()
-for e in ra:
-    print(e)
+print('a', list(ra))
+dd = defaultdict(list)
+# for e in ra:
+#     all_keys.add(e['ak'])
+#     dd.append[e['ak']]
+# for e in rb:
+#     all_keys.add(e['bk'])
+#
+# for e in rc:
+#     all_keys.add(e['ck'])
+
+print(sorted(all_keys))
+dd.update(ra)
+print((dd))
